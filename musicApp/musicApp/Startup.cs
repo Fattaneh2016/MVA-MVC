54fc9b0c3ce8eccbc7c1989f6f785cc933e88372
Fattaneh2016 <fatameri@hotmail.com>
2017-01-08 21:11:42 -0500

    ok

---
+++ musicApp/musicApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(musicApp.Startup))]
+namespace musicApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
